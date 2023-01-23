c1bb9d8d43ec090ba18062a8afb9143e2ced5635
Julia Kalazans <juliakalazanslm@gmail.com>
2023-01-23 09:37:55 -0300

    correcao de bug 2

--- project2/index.js
+++ project2/index.js
@@ -34,6 +34,7 @@ function getRandomCard() {
 
 function startGame() {
     isAlive = true;
+    hasBlackJack = false;
     let firstCard = getRandomCard();
     let secondCard = getRandomCard();
     cards = [firstCard, secondCard];
