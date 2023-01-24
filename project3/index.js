709a42738a5a8b543588c40746aa58109c4d039d
Julia Kalazans <109919694+juliaklzs@users.noreply.github.com>
2023-01-24 16:03:59 -0300

    codigo atualizado funcionando

--- project3/index.js
+++ project3/index.js
@@ -2,19 +2,17 @@ const inputBtn = document.querySelector("#input-btn");
 const inputEl = document.querySelector("#input-el");
 const ulEl = document.querySelector("#ul-el");
 let myLinks = [];
-let listItems = "";
 
 inputBtn.addEventListener("click", function() {
-    pushLink();
+    myLinks.push(inputEl.value);
+    renderLeads();
+    inputEl.value = "";
 })
 
-function pushLink() {
-    myLinks.push(inputEl.value);
-    
+function renderLeads() {
+    let listItems = "";
+    for (let i=0; i < myLinks.length; i++) {
+        listItems += "<li>" + myLinks[i] + "</li>";
+    }
+    ulEl.innerHTML = listItems;
 }
-
-for (let i=0; i < myLinks.length; i++) {
-    listItems += "<li>" + myLinks[i] + "</li>";
-}
-
-ulEl.innerHTML = listItems;
